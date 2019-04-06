5bbf75bd6a22203806f41c8c1b5d6c13a48229d7
Sheldon <thesheldon@googlemail.com>
2019-04-06 20:49:41 +0300

    other files

---
+++ models/userModel.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true, select: false }
+})
+module.exports = mongoose.model('User', UserSchema)
