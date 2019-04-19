f4f7acf7674acfd5452beb7e5e0f4731f8b9e107
Sheldon <thesheldon@googlemail.com>
2019-04-19 11:27:51 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         app: './react/index.js',
     },
@@ -40,7 +40,7 @@ module.exports = {
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('development')
+                'NODE_ENV': JSON.stringify('production')
             }
         })
     ],
